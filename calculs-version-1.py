2bae9b81af541d1193b470ed93507f006ae8cfe5
clem6404 <clem.cartier04@gmail.com>
2023-06-14 15:54:59 +0200

    oh

---
+++ calculs-version-1.py
@@ -0,0 +1,61 @@
+import multiprocessing as mp
+import time, random
+
+
+def donne_des_calculs(nb_expr):
+
+    caracteres=["*","/","+","-"]
+    for _ in range(nb_expr):
+        calcul = ""
+        chiffre1 = random.randrange(0, 1000, 1)
+        chiffre2 = random.randrange(0, 1000, 1)
+        operateur = random.randrange(0, 4, 1)
+        calcul = calcul + str(chiffre1)
+        calcul = calcul + " " + caracteres[operateur] + " "
+        calcul = calcul + str(chiffre2)
+        queueCalculs.put(calcul)
+        print(f"Je demande {calcul}")
+    fini.value = True
+    
+def calcule():
+    keep_running = True
+    print(fini.value)
+    while keep_running:
+        try:
+            calcul = queueCalculs.get(timeout=1)
+        except:
+            if fini.value == True:
+                keep_running = False
+            continue
+        res =  str(eval(calcul))
+        print(f"Le calculateur {mp.current_process().name} a calculé {calcul} = {res}")
+        time.sleep(0.5)
+        UniqueQueueRes.put(res)
+        
+
+
+if __name__ == '__main__':
+
+    nb_calculateurs = mp.cpu_count()//2
+    mes_calculateurs = []
+    
+    nb_demandeurs = mp.cpu_count() - nb_calculateurs
+    nb_expr = 5
+    
+    queueCalculs = mp.Queue()
+    UniqueQueueRes = mp.Queue()
+    
+    fini = mp.Value("b", False) 
+    
+    
+    demandeur = mp.Process(target=donne_des_calculs, args=(nb_expr,))
+    demandeur.start()
+    
+    
+    for _ in range(nb_calculateurs):
+        calculateurs = mp.Process(target=calcule, name=f"{_}")
+        calculateurs.start()
+        mes_calculateurs.append(calculateurs)
+
+    for p in mes_calculateurs: p.join()
+    demandeur.join()
--- calculs-version-2-multi.py
+++ calculs-version-2-multi.py
@@ -13,27 +13,27 @@ def donne_des_calculs(nb_expr, identifiant):
         calcul = calcul + str(chiffre1)
         calcul = calcul + caracteres[operateur]
         calcul = calcul + str(chiffre2)
-        queueCalculs.put((calcul, identifiant))
+        UniqueQueueCalculs.put((calcul, identifiant))
         print(f"Je suis le client {identifiant} et je demande {calcul}")
 
         res = les_queues[identifiant].get()
-        print(f"j'ai mon resultat {res}")
+        print(f"    Je suis le client {identifiant} et j'ai mon resultat {res}")
     
-    # Lorsqu'un demandeur a fini d'envoyer ses calculs il décrémente fini pour éviter le problème avec get dans une queue (queueCalculs) qui pourrait
+    # Lorsqu'un demandeur a fini d'envoyer ses calculs il décrémente fini pour éviter le problème avec get dans une queue (UniqueQueueCalculs) qui pourrait
     # etre vide
     fini.value -= 1
     
 def calcule():
     keep_running = True
-    while keep_running:     
+    while keep_running:
         try:
-            calcul, identifiant = queueCalculs.get(timeout=1) # leve le blocage de get au bout dune seconde si rien a get et passe dans except
+            calcul, identifiant = UniqueQueueCalculs.get(timeout=1) # leve le blocage de get au bout dune seconde si rien a get et passe dans except
         except:
-            if fini.value == 0:
+            if fini.value == 0: # on regarde si tout les demandeurs on envoyés leurs requetes
                 keep_running = False
             continue
         res =  str(eval(calcul))
-        print(f"Un serveur a calculé {calcul} = {res}")
+        print(f"  Le process {mp.current_process().name} a calculé {calcul} = {res}")
         time.sleep(1)
         les_queues[identifiant].put(res)
         
@@ -42,37 +42,34 @@ def calcule():
 if __name__ == '__main__':
 
     nb_calculateurs = 5
-    mes_esclaves = []
+    mes_calculateurs = []
     mes_demandeurs = []
-    queueCalculs = mp.Queue()
+    UniqueQueueCalculs = mp.Queue()
     nb_demandeurs = 10
     nb_expr = 4
     les_queues = []
     
     # on initialise fini egale aux nb de demandeurs
-    fini = mp.Value("i", nb_demandeurs) 
+    fini = mp.Value("i", nb_demandeurs)
     
-    
+    # une queue des résultats par demandeur
     for i in range(nb_demandeurs):
         queueRes = mp.Queue()
         les_queues.append(queueRes)
 
+    # les process demandeurs
     for i in range(nb_demandeurs):
         demandeur = mp.Process(target=donne_des_calculs, args=(nb_expr,i))
         demandeur.start()
         mes_demandeurs.append(demandeur)
 
-    
-    for _ in range(nb_calculateurs):
-        calculateurs = mp.Process(target=calcule)
+    # les process calculateurs
+    for i in range(nb_calculateurs):
+        calculateurs = mp.Process(target=calcule, name=f"calculateur {i}")
         calculateurs.start()
-        mes_esclaves.append(calculateurs)
+        mes_calculateurs.append(calculateurs)
 
     
 
-    for p in mes_esclaves: p.join()
+    for p in mes_calculateurs: p.join()
     for p in mes_demandeurs: p.join()
-
-    
-
-    
